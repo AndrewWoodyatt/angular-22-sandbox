4861d2c6f79ffae1da94ede2f5540252dfc7a84d
Andrew Woodyatt <andreww@eftsure.com.au>
2026-06-24 15:26:47 +1000

    Fix unit tests

--- angular-sandbox/src/app/session-history/session-history.spec.ts
+++ angular-sandbox/src/app/session-history/session-history.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { SessionHistory } from './session-history';
 
@@ -9,6 +10,7 @@ describe('SessionHistory', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SessionHistory],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SessionHistory);
